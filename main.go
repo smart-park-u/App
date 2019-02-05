7fdaf0ddbb5f772a74aa41fa17c2fe62f2f1ade8
Chad Johnerson <cjohn@ip-172-31-28-223.us-east-2.compute.internal>
2019-02-05 22:24:56 +0000

    First iteration of the main.go file to be used for handling http requests. This go instance also works with the database to return the requested data to the app/user.

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"net/http"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"
+)
+
+type Core struct {
+	DB *sql.DB
+}
+
+func main() {
+	c := &Core{
+		DB: dbConn(),
+	}
+
+	e := echo.New()
+	e.GET("/lots/:lot", c.getLotInfo)
+	e.PUT("/spots/:spotOccupied", c.updateSpot)
+	e.Logger.Fatal(e.Start(":12547"))
+	defer c.DB.Close()
+}
+
+type ParkingSpot struct {
+    lot_name string
+    spot_id string
+    is_occupied bool
+    spot_type string
+    location string
+}
+
+//function used to handle database connection
+func dbConn() (db *sql.DB) {
+    dbDriver := "mysql"
+    dbUser := "appuser"
+    dbPass := "password"
+    dbName := "smartparkuDB"
+    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+    if err != nil {
+        panic(err.Error())
+    }
+    return db
+}
+
+func (c *Core) getLotInfo(ctx echo.Context) error {
+	lot := ctx.Param("lot")
+	inqForm, err := c.DB.Query(fmt.Sprintf("SELECT COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'F' THEN 1 END) as openF, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'E' THEN 1 END) as openE, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'R' THEN 1 END) as openR, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'SB' THEN 1 END) as openSB, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'Handicap' THEN 1 END) as openHandicap FROM parking_lot;", lot, lot, lot, lot, lot))
+	if err != nil{
+		panic(err.Error())
+	}
+	var openF, openE, openR, openSB, openHandicap int
+	inqForm.Next()
+	if err := inqForm.Scan(&openF, &openE, &openR, &openSB, &openHandicap); err != nil{
+		fmt.Println(err)
+	}
+	resp := map[string]int{"F": openF, "E": openE, "R": openR, "SB": openSB, "Handicap": openHandicap}
+	return ctx.JSON(http.StatusOK, resp)
+}
+
+func (c *Core) updateSpot(ctx echo.Context) error {
+	spotOccupied := ctx.Param("spotOccupied")
+	spotID := ctx.Body("spotID")
+	location := ctx.Body("location")
+	lotName := ctx.Body("lotName")
+	if spotOccupied == "1"{
+		updForm, err := c.DB.Prepare("UPDATE parking_lot SET is_occupied = 1 WHERE spot_id = ? && lot_name = ? && location = ?")
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	if spotOccupied == "0"{
+		updForm, err := c.DB.Prepare("UPDATE parking_lot SET is_occupied = 0 WHERE spot_id = ? && lot_name = ? && location = ?")
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+                updForm.Exec(spotID, lotName, location)
+		resp :=  spotID + " has been updated in " + lotName + " lot on the following campus: " + location
+	return ctx.String(http.StatusOK, resp)
+}
