3501096f45dc8ab88931c8a0db3b4f9856d37ec3
Chad Johnerson <cjohn@ip-172-31-28-223.us-east-2.compute.internal>
2019-02-05 22:32:02 +0000

    Added comments to the methods. Also made some changes to the updateSpot Method to run different queries dependent on the value of isOccupied

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ type ParkingSpot struct {
     location string
 }
 
-//function used to handle database connection
+//Method used to handle database connection
 func dbConn() (db *sql.DB) {
     dbDriver := "mysql"
     dbUser := "appuser"
@@ -45,6 +45,7 @@ func dbConn() (db *sql.DB) {
     return db
 }
 
+//Method that returns every parking spot type and the number of available spots for each type in the requested lot that the user clicks on in their application.
 func (c *Core) getLotInfo(ctx echo.Context) error {
 	lot := ctx.Param("lot")
 	inqForm, err := c.DB.Query(fmt.Sprintf("SELECT COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'F' THEN 1 END) as openF, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'E' THEN 1 END) as openE, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'R' THEN 1 END) as openR, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'SB' THEN 1 END) as openSB, COUNT(CASE WHEN lot_name = '%s' && is_occupied = 0 && spot_type = 'Handicap' THEN 1 END) as openHandicap FROM parking_lot;", lot, lot, lot, lot, lot))
@@ -56,10 +57,12 @@ func (c *Core) getLotInfo(ctx echo.Context) error {
 	if err := inqForm.Scan(&openF, &openE, &openR, &openSB, &openHandicap); err != nil{
 		fmt.Println(err)
 	}
+	//formats the response string for the application to be able to parse and use on screen.
 	resp := map[string]int{"F": openF, "E": openE, "R": openR, "SB": openSB, "Handicap": openHandicap}
 	return ctx.JSON(http.StatusOK, resp)
 }
 
+//Method used to update a specific spot to whether it's occupied or not.
 func (c *Core) updateSpot(ctx echo.Context) error {
 	spotOccupied := ctx.Param("spotOccupied")
 	spotID := ctx.Body("spotID")
